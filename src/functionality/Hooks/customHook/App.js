4ddfebc2b9a67c9bcee03dbb1302c78928143f71
krehicbenjamin <krehicbenjamin@gmail.com>
2021-07-18 20:00:11 +0200

    custom hook practice code

---
+++ src/functionality/Hooks/customHook/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import useCounter from "./useCounter"
+
+function App() {
+    const [number, add] = useCounter()
+    
+    return (
+        <div>
+            <h1>The count is {number}</h1>
+            <button onClick={add}>Add 1</button>
+        </div>
+    )
+}
+
+export default App
