aa5e5e8cbf5456f8f6725d09cef3d3cf18c01910
krehicbenjamin <krehicbenjamin@gmail.com>
2021-07-18 17:53:13 +0200

    converting ThemeContext to be a functional component

--- src/functionality/Hooks/useContext/ThemeContext.js
+++ src/functionality/Hooks/useContext/ThemeContext.js
@@ -1,28 +1,22 @@
-import React, {Component} from "react"
-
-
+import React, {useState} from "react"
 const ThemeContext = React.createContext()
 
-class ThemeContextProvider extends Component {
-    state = {
-        theme: "dark"
+
+function ThemeContextProvider(props){
+   
+    const [theme, setTheme] = useState("dark")
+    
+    const toggleTheme = () => {
+        setTheme(theme === "light" ? "dark" : "light")
     }
     
-    toggleTheme = () => {
-        this.setState(prevState => {
-            return {
-                theme: prevState.theme === "light" ? "dark" : "light"
-            }
-        })
-    }
     
-    render() {
         return (
-            <ThemeContext.Provider value={{theme: this.state.theme, toggleTheme: this.toggleTheme}}>
-                {this.props.children}
+            <ThemeContext.Provider value={{theme: theme, toggleTheme: toggleTheme}}>
+                {props.children}
             </ThemeContext.Provider>
         )
-    }
+    
 }
 
 export {ThemeContextProvider, ThemeContext}
